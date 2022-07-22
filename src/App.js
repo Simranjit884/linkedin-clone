42c66e7eb411b42a30dfdba9d73328a45f829fc1
samy884 <ssgill884@gmail.com>
2022-07-22 18:01:20 +0530

    created header component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import Header from './components/Header';
 
 function App() {
   return (
     <div className="App">
       <h1> This is a heading for linkedin clone.</h1>
+                {/* Header */}
+                <Header />
+      {/*         App body            */}
+            {/* Left Sidebar */}
+            {/* Feed */}
+            {/* Right side widgets*/}
     </div>
   );
 }
